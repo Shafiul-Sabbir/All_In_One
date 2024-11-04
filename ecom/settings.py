fac7b14c4308a787e42d5dd9ccaecfd057c1083a
Shafiul-Sabbir <sabbirvai82@gmail.com>
2024-11-04 13:57:30 +0600

    set allowed host

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = 'django-insecure-df_c!2lc37o5dwg*$nj%3yl$7zsdt@c)q%=b5dmy3do+k44+tw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['allinone-production.up.railway.app', 'https://allinone-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['allinone-production.up.railway.app', 'https://allinone-production.up.railway.app']
 
 
 # Application definition
