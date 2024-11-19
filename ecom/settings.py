5c27138c3377cfba4f6e4b3a3a447da6d82500f3
Shafiul-Sabbir <sabbirvai82@gmail.com>
2024-11-20 02:57:20 +0600

    debug remains true due to pucture issues

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-df_c!2lc37o5dwg*$nj%3yl$7zsdt@c)q%=b5dmy3do+k44+tw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['allinone-production.up.railway.app', 'https://allinone-production.up.railway.app' ]
 CSRF_TRUSTED_ORIGINS = ['https://allinone-production.up.railway.app']
 # the domain as ALLOWED_HOSTS has found from "public networking" under "networking" under "settings" in the main app of railway, not in database section.
